4c78ec237e658cd77e06bbae7635dac0eec3ebe0
Akbar Ridho Arrafi <akbarridhoarrafi@gmail.com>
2026-06-27 10:59:59 +0700

    Update: Add a Vercel error trap

--- api/index.php
+++ api/index.php
@@ -4,23 +4,28 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/../vendor/autoload.php';
+try {
+    require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Bypass sistem file Read-Only Vercel dengan menggunakan folder sementara (/tmp)
-$storagePath = '/tmp/storage';
+    $storagePath = '/tmp/storage';
 
-if (!is_dir($storagePath)) {
-    mkdir($storagePath, 0777, true);
-    mkdir($storagePath . '/framework/cache', 0777, true);
-    mkdir($storagePath . '/framework/views', 0777, true);
-    mkdir($storagePath . '/framework/sessions', 0777, true);
-    mkdir($storagePath . '/logs', 0777, true);
+    if (!is_dir($storagePath)) {
+        @mkdir($storagePath, 0777, true);
+        @mkdir($storagePath . '/framework/cache', 0777, true);
+        @mkdir($storagePath . '/framework/views', 0777, true);
+        @mkdir($storagePath . '/framework/sessions', 0777, true);
+        @mkdir($storagePath . '/logs', 0777, true);
+    }
+
+    $app->useStoragePath($storagePath);
+    $_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
+    putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
+
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>Vercel PHP Fatal Error</h1>";
+    echo "<pre>" . (string) $e . "</pre>";
 }
-
-$app->useStoragePath($storagePath);
-$_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
-putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
-
-$app->handleRequest(Request::capture());
