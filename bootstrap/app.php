ea7dc8b232d8fa92c6ee2bee05740756af7b2a63
Akbar Ridho Arrafi <akbarridhoarrafi@gmail.com>
2026-06-27 11:04:34 +0700

    Update: Catch the original error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->report(function (\Throwable $e) {
+            echo "<h1>ORIGINAL ERROR:</h1><pre>" . (string) $e . "</pre>";
+            die();
+        });
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
